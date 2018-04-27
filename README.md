82c7d6ff0e30387c2adf676f49feacf07e556430
Nguyen Nguyen <nguyen.nguyen@here.com>
2018-04-26 23:25:13 -0500

    update `swing` to `s`

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # Swing
 Folder navigator, swing between folders by their name.
 
-## Setup
+### Setup
 1. Download _setup\_swing.sh_ somewhere (e.g. ~/.scripts/setup\_swing.sh)
-2. Add to _~/.bash\_rc_: `source ~/.scripts/setup_swing.sh`
+2. Add to _~/.bashrc_: `source ~/.scripts/setup_swing.sh`
 
-## Usage
-1. `swing init [name]` add current folder, defaults to folder name
-2. `swing <folder name>` cd to folder
+### Usage
+1. `s init [name]` add current folder, defaults to folder name
+2. `s <folder name>` cd to folder
 
-## TODO
+### TODO
 - Add a clean method to remove obselete paths
--- setup_swing.sh
+++ setup_swing.sh
@@ -2,94 +2,99 @@
 
 # Setup
 if [[ ! -d $HOME/.swing ]]; then
-    root_dir="$HOME/.swing"
-    mkdir -p $root_dir
+  root_dir="$HOME/.swing"
+  mkdir -p $root_dir
 
-    touch $root_dir/paths
-    touch $root_dir/names
-    touch $root_dir/dir_to_cd
+  touch $root_dir/paths
+  touch $root_dir/names
+  touch $root_dir/dir_to_cd
+
+  unset root_dir
 fi
 
 # Completion
 function __swing_completion() {
-    local cur prev opts
+  local cur prev opts
 
-    COMPREPLY=()
-    cur="${COMP_WORDS[COMP_CWORD]}"
-    prev="${COMP_WORDS[COMP_CWORD-1]}"
-    opts=$(awk -F'=' '{print $2}' $HOME/.swing/names | tr "\n" " ")
+  COMPREPLY=()
+  cur="${COMP_WORDS[COMP_CWORD]}"
+  prev="${COMP_WORDS[COMP_CWORD-1]}"
+  opts=$(awk -F'=' '{print $2}' $HOME/.swing/names | tr "\n" " ")
 
-    if [[ ${prev} == "swing" ]] ; then
-        COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
-        return 0
-    fi
+  if [[ ${prev} == "swing" ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" ${cur}) )
+    return 0
+  fi
 }
 complete -F __swing_completion swing
 
 
-# This is sourced to perform "cd" in the current shell session
-function swing() {
-    (
-        FOLDER_PREFIX="__dir_"
-        action=$1
-        root_dir="$HOME/.swing"
-        paths_file="$root_dir/paths"
-        names_file="$root_dir/names"
-        cd_path="$root_dir/dir_to_cd"
+function s() {
+  # The swing function
+  # This is sourced to perform "cd" in the current shell session
 
-        function __swing_get_folder_id() {
-            local folder_name folder_slug folder_id
+  (
+    FOLDER_PREFIX="__dir_"
+    action=$1
+    root_dir="$HOME/.swing"
+    paths_file="$root_dir/paths"
+    names_file="$root_dir/names"
+    cd_path="$root_dir/dir_to_cd"
 
-            folder_name=$1
-            folder_slug=$(echo $folder_name | xargs | tr " .-" "_" | tr A-Z a-z)
-            folder_id="$FOLDER_PREFIX$folder_slug"
+    function __swing_get_folder_id() {
+      local folder_name folder_slug folder_id
 
-            echo $folder_id
-        }
+      folder_name=$1
+      folder_slug=$(echo $folder_name | xargs | tr " .-" "_" | tr A-Z a-z)
+      folder_id="$FOLDER_PREFIX$folder_slug"
 
-        function __swing_get_folder_path() {
-            local folder_id folder_path
+      echo $folder_id
+    }
 
-            source $paths_file
+    function __swing_get_folder_path() {
+      local folder_id folder_path
 
-            folder_id=$1
-            folder_path="${!folder_id}"
-            echo $folder_path
-        }
+      source $paths_file
 
-        function __swing_init() {
-            local folder_path folder_name folder_id old_path
+      folder_id=$1
+      folder_path="${!folder_id}"
+      echo $folder_path
+    }
 
-            input_name=$1
-            folder_path=$(pwd)
-            folder_name=$([[ ! -z $input_name ]] && echo $input_name || basename $folder_path)
-            folder_id=$(__swing_get_folder_id $folder_name)
-            old_path=$(__swing_get_folder_path $folder_id)
+    function __swing_init() {
+      local folder_path folder_name folder_id old_path
 
-            echo "$folder_id=$folder_path" >> $paths_file
-            echo "$folder_id=$folder_name" >> $names_file
-            echo "Created \"$folder_name\" for path [$folder_path]"
-        }
+      input_name=$1
+      folder_path=$(pwd)
+      folder_name=$([[ ! -z $input_name ]] && echo $input_name || basename $folder_path)
+      folder_id=$(__swing_get_folder_id $folder_name)
+      old_path=$(__swing_get_folder_path $folder_id)
 
-        function __swing_write_to_cd() {
-            local folder_name folder_id folder_path
+      echo "$folder_id=$folder_path" >> $paths_file
+      echo "$folder_id=$folder_name" >> $names_file
+      echo "Created \"$folder_name\" for path [$folder_path]"
+    }
 
-            folder_name=$1
-            folder_id=$(__swing_get_folder_id $folder_name)
-            folder_path=$(__swing_get_folder_path $folder_id)
+    function __swing_write_to_cd() {
+      local folder_name folder_id folder_path
 
-            echo "$folder_path" > $cd_path
-        }
+      folder_name=$1
+      folder_id=$(__swing_get_folder_id $folder_name)
+      folder_path=$(__swing_get_folder_path $folder_id)
 
-        if [[ $action == "init" ]]; then
-            __swing_init $2
-        else
-            __swing_write_to_cd $action
-        fi
-    )
+      echo "$folder_path" > $cd_path
+    }
 
-    if [[ ! $1 == "init" ]]; then
-        dir_to_cd=$(cat $HOME/.swing/dir_to_cd)
-        test ! -z $dir_to_cd && cd $dir_to_cd
+    if [[ $action == "init" ]]; then
+      __swing_init $2
+    else
+      __swing_write_to_cd $action
     fi
+  )
+
+  if [[ ! $1 == "init" ]]; then
+    dir_to_cd=$(cat $HOME/.swing/dir_to_cd)
+    test ! -z $dir_to_cd && cd $dir_to_cd
+    unset dir_to_cd
+  fi
 }
